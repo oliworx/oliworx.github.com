24794d3d6b7a75a0556abd46b9af006bf17f9b71
Oliver Kurmis <oliver@kurmis.com>
2019-08-16 23:35:11 +0200

    Fix casperjs test

--- tests/casperjs-website-tests.js
+++ tests/casperjs-website-tests.js
@@ -2,7 +2,7 @@ casper.test.begin('kurmis.com homepage', 8, function suite(test) {
   casper.start("https://www.kurmis.com/", function() {
     test.assertTitle("Oliver Kurmis", "kurmis.com homepage title is the one expected");
 	test.assertElementCount('#gallery img', 4, "there are 4 gallery images just as expected");
-	test.assertElementCount('h2', 3, "there are 3 H2 headings just as expected");
+	test.assertElementCount('h2', 4, "there are 4 H2 headings just as expected");
 	test.assertResourceExists('d2c89a8-200.jpg', 'Image Beijing confirmed');
 	test.assertResourceExists('c190c7f-200.jpg', 'Image Munich confirmed');
 	test.assertResourceExists('6a8fb78-200.jpg', 'Image OK-Karate confirmed');
